d7c9e6c98bca787d967ab5911f0275faf809150a
Deng Yi <yi.deng@yideng-mac.local>
2019-07-05 16:08:38 +0800

    add standard benchmark

--- benchall.sh
+++ benchall.sh
@@ -4,14 +4,14 @@ PYTHON=${PYTHON:=python}
 
 idc=$1
 
-$PYTHON bench_hub.py -o result_${idc}.json
-$PYTHON bench_local.py --append result_${idc}.json
-$PYTHON bench_pool.py --append result_${idc}.json
-$PYTHON bench_queue.py --append result_${idc}.json
-$PYTHON bench_sendall.py --append result_${idc}.json
-$PYTHON bench_sleep0.py --append result_${idc}.json
-$PYTHON bench_socket.py --append result_${idc}.json
-$PYTHON bench_spawn.py --append result_${idc}.json
-$PYTHON bench_subprocess.py --append result_${idc}.json
-$PYTHON bench_threadpool.py --append result_${idc}.json
-$PYTHON bench_tracer.py --append  result_${idc}.json
+$PYTHON bench_hub.py --quiet -o result_${idc}.json
+$PYTHON bench_local.py --quiet --append result_${idc}.json
+$PYTHON bench_pool.py --quiet --append result_${idc}.json
+$PYTHON bench_queue.py --quiet --append result_${idc}.json
+$PYTHON bench_sendall.py --quiet --append result_${idc}.json
+$PYTHON bench_sleep0.py --quiet --append result_${idc}.json
+$PYTHON bench_socket.py --quiet --append result_${idc}.json
+$PYTHON bench_spawn.py --quiet --append result_${idc}.json
+$PYTHON bench_subprocess.py --quiet --append result_${idc}.json
+$PYTHON bench_threadpool.py --quiet --append result_${idc}.json
+$PYTHON bench_tracer.py --quiet --append result_${idc}.json
